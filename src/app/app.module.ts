50d17af9c24431c3ed8e495b2aedf953de6c85ac
Rupam <roopamroy@gmail.com>
2017-09-08 08:21:14 -0400

    Done with phase 1 where music is playing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,15 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Media } from '@ionic-native/media';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -22,12 +21,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Media,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { Media, MediaObject } from '@ionic-native/media';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{ title: string, note: string, icon: string }>;
+  music: MediaObject;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public media: Media, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
 
+    // Let's populate this page with some filler content for funzies
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+      'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+
+  ionViewDidLoad(){
+    this.music = this.media.create('https://s3.amazonaws.com/rupam-bucket-1/BashBagan.mp3');
+    this.music.onStatusUpdate.subscribe(status => console.log(status)); // fires when file status changes
+
+    this.music.onSuccess.subscribe(() => console.log('Action is successful'));
+
+    this.music.onError.subscribe(error => console.log('Error!', error));
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    item.title = 'tapped';
+    
+    this.music.play();
+  }
+
+  itemKeyUp(event, item) {
+    item.title = 'untapped';
+    this.music.stop();
   }
 
 }
